e1626b8c6c39bc2e6ae08c10bda86b4558a067cb
Orien Zhang <zhangorienspam@gmail.com>
2026-05-01 16:13:29 -0700

    codex added some things to fix migration errors

--- scripts/lib/load-script-env.ts
+++ scripts/lib/load-script-env.ts
@@ -1,45 +1,13 @@
 import * as nextEnv from '@next/env'
 
+import { preferHostedDatabaseURLForVercelEnvRun } from '../../src/utilities/resolveDatabaseURL'
+
 type NextEnvModule = typeof nextEnv & {
   default?: typeof nextEnv
 }
 
 const { loadEnvConfig } =
   'loadEnvConfig' in nextEnv ? nextEnv : ((nextEnv as NextEnvModule).default as typeof nextEnv)
-
-const localDatabaseHosts = new Set(['127.0.0.1', 'localhost', '::1'])
-
-const isLocalDatabaseURL = (value: string | undefined) => {
-  if (!value) {
-    return false
-  }
-
-  try {
-    const url = new URL(value)
-
-    return localDatabaseHosts.has(url.hostname)
-  } catch {
-    return false
-  }
-}
-
-const preferHostedDatabaseURLForVercelEnvRun = () => {
-  if (process.env.VERCEL !== '1') {
-    return
-  }
-
-  if (process.env.DATABASE_URL && !isLocalDatabaseURL(process.env.DATABASE_URL)) {
-    return
-  }
-
-  const hostedDatabaseURL = process.env.NEON_POSTGRES_URL || process.env.NEON_DATABASE_URL
-
-  if (!hostedDatabaseURL) {
-    return
-  }
-
-  process.env.DATABASE_URL = hostedDatabaseURL
-}
 
 export const loadScriptEnv = () => {
   const injectedEnv = { ...process.env }
